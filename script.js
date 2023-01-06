bae2d11ecc9cd7d4d27e80688da3d7140be9def7
Michael Norman <mikenorman192@msn.com>
2023-01-06 19:25:28 +0000

    inital

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container');
+
+const gridDiv = document.createElement('div');
+
+gridDiv.classList.add('gridDiv');
+
+gridDiv.textContent = 'This is the glorious text-content!';
+
+container.appendChild(gridDiv);
+
