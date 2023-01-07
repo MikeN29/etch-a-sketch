ead33b9fab7d9cd2dc2cfaf32e96972ebcd2f66a
Michael Norman <mikenorman192@msn.com>
2023-01-07 16:02:08 +0000

    attempt to add class change on mouseover

--- script.js
+++ script.js
@@ -7,10 +7,27 @@ for (let i = 0; i < 256; i++) {
     const gridDiv = document.createElement('div');
     
     gridDiv.classList.add('gridDiv');
+    gridDiv.setAttribute("id", "gridDiv");
     
     container.appendChild(gridDiv);
 
+
+
 }
+
+let cell = document.getElementById("gridDiv");
+cell.addEventListener("mouseenter", (event) => {
+    // highlight the mouseenter target
+    event.cell.style.backgroundColor = "purple";
+    console.log('hello mouse over');
+
+}, false); 
+
+
+
+//cell.addEventListener("mouseout", func1, false);
+
+
 
 
 
